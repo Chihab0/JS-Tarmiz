2aa694e304817fe45edf7fde1983faa83bf40545
Chihab0 <yc.chikhaoui@esi-sba.dz>
2024-02-23 21:55:28 +0100

    Fetching using Axios(considering server breakdowns pottential errors)

--- usersPosts/jsonPlaceHolderProject.js
+++ usersPosts/jsonPlaceHolderProject.js
@@ -1,12 +1,14 @@
-usersFilling= ()=>{
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res)=>res.json())
-    .then((json)=>{
-        json.forEach((ele) => {
+usersFilling = ()=>{
+    axios.get("https://jsonplaceholder.typicode.com/users")
+    .then((res)=>{
+        res.data.forEach((ele) => {
             document.getElementById("users").innerHTML+=
                 `<button class="user" onclick="clicked(this); postsLoading(${ele.id})"><span>${ele.name}</span><br>${ele.email}</button>`
         });
-    });
+    })
+    .catch(()=>{
+        document.getElementById("container").innerHTML=`<h1 style="display: flex; justify-content: center;">Users can't be loaded the server is BROKEN!</h1>`
+    })
 }
 clicked= (self) => {
     const users = document.getElementsByClassName("user");
@@ -16,12 +18,11 @@ clicked= (self) => {
     }
     self.classList.add('clicked');
 }
-postsLoading = (id)=>{
-    fetch("https://jsonplaceholder.typicode.com/posts?userId="+id)
-    .then((res)=>res.json())
-    .then((json)=>{
+postsLoading= (id)=>{
+    axios.get("https://jsonplaceholder.typicode.com/posts?userId="+id)
+    .then(res=>{
         document.getElementById("container").innerHTML="";
-        json.forEach(ele =>{
+        res.data.forEach((ele)=>{
             document.getElementById("container").innerHTML+=
                 `
                 <div class="posts">
@@ -31,6 +32,9 @@ postsLoading = (id)=>{
                 </div>
                 `
         })
+    })
+    .catch(()=>{
+        document.getElementById("container").innerHTML=`<h1 style="display: flex; justify-content: center;">Posts can't be loaded the server is BROKEN</h1>`
     })
 }
 usersFilling();
